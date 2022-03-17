c1dbeb7de7250749204c2d9831e09aa6385126da
Li Hongyu <lihongyu1999@bupt.edu.cn>
2022-03-17 10:02:53 +0000

    vga: a safe mode
    provide a safe interface for vga

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod vga_buffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) ->! {
+    println!("{}", _info);
     loop{}
 }
 
@@ -13,15 +14,7 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    loop {}
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
+    loop{}
 }
-
